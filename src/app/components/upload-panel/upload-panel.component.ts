58cebae7bb6ff785cf7ba77d6d12d83d93fc5696
shoomkloom <shoomkloom@gmail.com>
2025-07-18 10:20:53 +0300

    Support environment Support ellipses and tooltips in text fields

--- src/app/components/upload-panel/upload-panel.component.ts
+++ src/app/components/upload-panel/upload-panel.component.ts
@@ -1,17 +1,20 @@
+import { NgFor, NgIf } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-upload-panel',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf, NgFor],
   templateUrl: './upload-panel.component.html',
   styleUrl: './upload-panel.component.css'
   })
 export class UploadPanelComponent {
   webLink: string = '';
   selectedFileName: string = '';
+  //@@differences: string[] = [];
 
   constructor(private http: HttpClient) {}
 
@@ -25,7 +28,7 @@ export class UploadPanelComponent {
       const formData = new FormData();
       formData.append('file', file);
 
-      this.http.post('http://localhost:3000/api/upload', formData).subscribe({
+      this.http.post(`${environment.serverUrl}/api/upload`, formData).subscribe({
         next: response => {
           console.log('Upload successful', response)
           this.selectedFileName = '';
@@ -39,7 +42,37 @@ export class UploadPanelComponent {
     }
   }
 
+  /*@@
   uploadLink() {
+    if (this.webLink.trim()) {
+      this.http.post<{message: string; differences?: string[];}>(`${environment.serverUrl}/api/link`, { link: this.webLink }).subscribe({
+        next: response => {
+          console.log('Link upload successful', response);
+
+          if (response.differences && Array.isArray(response.differences)) {
+            this.differences = response.differences;
+            alert('Link already exists. Differences were detected!');
+          } 
+          else {
+            this.webLink = '';
+            this.differences = [];
+            alert('Link uploaded successfully!');
+          }
+          
+        },
+        error: error => {
+          console.error('Link upload error', error);
+          alert(`Link upload failed!\n\n${error.message || error}`);
+        }
+      });
+    } 
+    else {
+      console.error('Please enter a valid link.');
+      alert('Please enter a valid link.');
+    }
+  }
+    @@*/
+    uploadLink() {
     if (this.webLink.trim()) {
       this.http.post('http://localhost:3000/api/link', { link: this.webLink }).subscribe({
         next: response => {
